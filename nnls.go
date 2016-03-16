a2a8aa62666412d49a9877887a542afb40213cee
Sonia Keys <skeys@cfa.harvard.edu>
2016-03-16 15:17:22 -0400

    simplistic, but working

--- nnls.go
+++ nnls.go
@@ -1,40 +1,47 @@
 package nnls
 
-import "log"
 import "github.com/skelterjohn/go.matrix"
 
-func NNLS(x [][]float64, y []float64, n int) ([]float64, error) {
+func NNLS(x [][]float64, y []float64, n int) ([]float64, int, error) {
 	A := matrix.MakeDenseMatrixStacked(x)
 	b := matrix.MakeDenseMatrix(y, len(y), 1)
 	β := make([]float64, len(x[0])) // return value
-	log.Println("β allocated:", β)
 	AT := A.Transpose()
 	m, err := AT.TimesDense(b)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	m.Scale(-1)
 	μ := m.Array()
-	log.Println("initial μ:", μ)
 	H, err := AT.TimesDense(A)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
-	Hd := H.DiagonalCopy() // diagonal as a slice
-	log.Println("Hd:", Hd)
-	HT := H.Transpose().Arrays() // H columns as slices
+	Hd := H.DiagonalCopy()
+	Ha := H.Arrays() // (H is symmetric)
+	if n < 0 {
+		n = 10000
+	}
 	for i := 0; i < n; i++ {
+		ch := false
 		for k, βk := range β {
 			b := βk - μ[k]/Hd[k]
 			if b < 0 {
 				b = 0
 			}
-			β[k] = b
+			if b != βk {
+				β[k] = b
+				ch = true
+			}
 			b -= βk
-			for i, h := range HT[k] {
-				μ[i] += b * h
+			for j, h := range Ha[k] {
+				μ[j] += b * h
 			}
 		}
+		if !ch {
+			n = i + 1
+			break
+		}
 	}
-	return β, nil
+	return β, n, nil
 }
--- nnls_test.go
+++ nnls_test.go
@@ -14,8 +14,34 @@ func ExampleNNLS() {
 		61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46}
 	A := make([][]float64, len(height))
 	for i, h := range height {
-		A[i] = []float64{h * h, h, 1}
+		A[i] = []float64{h * h, h, 1} // Vandermonde
 	}
-	fmt.Println(nnls.NNLS(A, weight, 7000))
+	β, n, _ := nnls.NNLS(A, weight, -1)
+	fmt.Printf("coefficents:  %.2f\n", β)
+	fmt.Println("iterations:  ", n)
+	fmt.Println("measured  modeled    error")
+	for i, h := range height {
+		m := h*(h*β[0]+β[1]) + β[2]
+		w := weight[i]
+		fmt.Printf("%8.2f %8.2f %8.2f\n", w, m, w-m)
+	}
 	// Output:
+	// coefficents:  [18.61 0.00 11.15]
+	// iterations:   7343
+	// measured  modeled    error
+	//    52.21    51.36     0.85
+	//    53.12    53.02     0.10
+	//    54.48    54.14     0.34
+	//    55.84    55.86    -0.02
+	//    57.20    57.02     0.18
+	//    58.57    58.79    -0.22
+	//    59.93    60.59    -0.66
+	//    61.29    61.81    -0.52
+	//    63.11    63.67    -0.56
+	//    64.47    64.93    -0.46
+	//    66.28    66.84    -0.56
+	//    68.10    68.14    -0.04
+	//    69.92    70.11    -0.19
+	//    72.19    71.44     0.75
+	//    74.46    73.47     0.99
 }
