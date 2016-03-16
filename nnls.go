9c62b7a91e1f88a7004a2ff41fc49b754ac62889
Sonia Keys <skeys@cfa.harvard.edu>
2016-03-16 17:11:11 -0400

    add KTT

--- nnls.go
+++ nnls.go
@@ -4,7 +4,7 @@ package nnls
 
 import "github.com/skelterjohn/go.matrix"
 
-func NNLS(x [][]float64, y []float64, n int) ([]float64, int, error) {
+func SCAIterate(x [][]float64, y []float64, n int) ([]float64, int, error) {
 	A := matrix.MakeDenseMatrixStacked(x)
 	b := matrix.MakeDenseMatrix(y, len(y), 1)
 	AT := A.Transpose()
@@ -16,7 +16,7 @@ func NNLS(x [][]float64, y []float64, n int) ([]float64, int, error) {
 	μ := m.Array()
 	H, _ := AT.TimesDense(A)
 	Hd := H.DiagonalCopy()
-	Ha := H.Arrays() // (H is symmetric)
+	Ha := H.Arrays()                // (H is symmetric)
 	β := make([]float64, len(x[0])) // return value
 	if n < 0 {
 		n = 10000
@@ -43,4 +43,46 @@ func NNLS(x [][]float64, y []float64, n int) ([]float64, int, error) {
 		}
 	}
 	return β, n, nil
+}
+
+func SCAKTT(x [][]float64, y []float64, ε float64) ([]float64, int, error) {
+	A := matrix.MakeDenseMatrixStacked(x)
+	b := matrix.MakeDenseMatrix(y, len(y), 1)
+	AT := A.Transpose()
+	m, err := AT.TimesDense(b)
+	if err != nil {
+		return nil, 0, err
+	}
+	m.Scale(-1)
+	μ := m.Array()
+	H, _ := AT.TimesDense(A)
+	Hd := H.DiagonalCopy()
+	Ha := H.Arrays()                // (H is symmetric)
+	β := make([]float64, len(x[0])) // return value
+	nε := -ε
+	i := 0
+i:
+	for ; ; i++ {
+		for k, βk := range β {
+			b := βk - μ[k]/Hd[k]
+			if b < 0 {
+				b = 0
+			}
+			β[k] = b
+			b -= βk
+			for j, h := range Ha[k] {
+				μ[j] += b * h
+			}
+		}
+		for k, m := range μ {
+			if m < nε {
+				continue i
+			}
+			if β[k] > 0 && m > ε {
+				continue i
+			}
+		}
+		break
+	}
+	return β, i + 1, nil
 }
--- nnls_test.go
+++ nnls_test.go
@@ -8,17 +8,20 @@ import (
 	"github.com/soniakeys/nnls"
 )
 
-func ExampleNNLS() {
-	// Wikipedia example data
-	height := []float64{1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63,
+// Wikipedia example data
+var (
+	height = []float64{1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63,
 		1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83}
-	weight := []float64{52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93,
+	weight = []float64{52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93,
 		61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46}
+)
+
+func ExampleSCAIterate() {
 	A := make([][]float64, len(height))
 	for i, h := range height {
 		A[i] = []float64{h * h, h, 1} // Vandermonde
 	}
-	β, n, _ := nnls.NNLS(A, weight, -1)
+	β, n, _ := nnls.SCAIterate(A, weight, -1)
 	fmt.Printf("coefficents:  %.2f\n", β)
 	fmt.Println("iterations:  ", n)
 	fmt.Println("measured  modeled    error")
@@ -46,4 +49,39 @@ func ExampleNNLS() {
 	//    69.92    70.11    -0.19
 	//    72.19    71.44     0.75
 	//    74.46    73.47     0.99
+}
+
+func ExampleSCAKTT() {
+	A := make([][]float64, len(height))
+	for i, h := range height {
+		A[i] = []float64{h * h, h, 1}
+	}
+	β, n, _ := nnls.SCAKTT(A, weight, .01)
+	fmt.Printf("coefficents:  %.2f\n", β)
+	fmt.Println("iterations:  ", n)
+	fmt.Println("measured  modeled    error")
+	for i, h := range height {
+		m := h*(h*β[0]+β[1]) + β[2]
+		w := weight[i]
+		fmt.Printf("%8.2f %8.2f %8.2f\n", w, m, w-m)
+	}
+	// Output:
+	// coefficents:  [18.60 0.00 11.16]
+	// iterations:   5951
+	// measured  modeled    error
+	//    52.21    51.36     0.85
+	//    53.12    53.02     0.10
+	//    54.48    54.14     0.34
+	//    55.84    55.86    -0.02
+	//    57.20    57.02     0.18
+	//    58.57    58.79    -0.22
+	//    59.93    60.59    -0.66
+	//    61.29    61.81    -0.52
+	//    63.11    63.67    -0.56
+	//    64.47    64.93    -0.46
+	//    66.28    66.84    -0.56
+	//    68.10    68.14    -0.04
+	//    69.92    70.11    -0.19
+	//    72.19    71.44     0.75
+	//    74.46    73.46     1.00
 }
